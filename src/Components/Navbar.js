283731d779673a0dbdfc76770bb2770f03cc067c
Ankit Bajpai <ankit65bajpai@gmail.com>
2023-03-21 07:55:06 +0530

    Modified navbar

--- src/Components/Navbar.js
+++ src/Components/Navbar.js
@@ -31,7 +31,7 @@ const Navbar = () => {
         // <div className="container-fluid">
         <nav className={`navbar navbar-expand-lg navbar-light ${scroll ? 'active' : ''}`}>
             <div className="container-fluid">
-                <Link to="/" className="brand fs-3 ms-5">Ulearn</Link>
+                <Link to="/" className="brand fs-3 ms-5">ULearn</Link>
                 <button className="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation">
                     <span className="navbar-toggler-icon"></span>
                 </button>
